1654b4bc8083517fedf9a942b5a8d2b69818071d
Himanshu Narang <himanshunarang15@gmail.com>
2018-09-11 18:16:53 +0530

    Add files via upload

---
+++ Reverse_a_linked_list_using_recursion.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+using namespace std;
+struct node{
+    int data;
+    struct node* next;
+};
+void reverselinkedlist(struct node *head){
+    if(head==NULL){
+        return;
+    }
+    reverselinkedlist(head->next);
+    cout<<head->data<<" ";
+}
+void push(struct node **head,int data){
+    struct node* new_node=(struct node*)malloc(sizeof(struct node));
+    new_node->data=data;
+    new_node->next=*head;
+    *head=new_node;
+}
+int main(){
+    struct node* head=NULL;
+    push(&head,4);
+    push(&head,3);
+    push(&head,2);
+    push(&head,1);
+    reverselinkedlist(head);
+    return 0;
+}
