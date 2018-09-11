9104ad8cc796edcfa39b6477ef0002f72f5603f7
Himanshu Narang <himanshunarang15@gmail.com>
2018-09-12 01:18:01 +0530

    Add files via upload

---
+++ count_frequency_of_no_in_an_array.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void count_frequency(int arr[],int n){
+    unordered_map<int,int>m;
+    for(int i=0;i<n;i++){
+        m[arr[i]]++;
+    }
+    for(auto it: m){
+        cout<<it.first<<" "<<it.second<<endl;
+    }
+}
+int main(){
+    int arr[]={10,20,20,10,10,5,10,20};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    count_frequency(arr,n);
+    return 0;
+}
