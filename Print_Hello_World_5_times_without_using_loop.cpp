5cdbd563a89b3fbcc3cc0e6e6e575fe20eba095d
Himanshu Narang <himanshunarang15@gmail.com>
2018-09-12 01:29:08 +0530

    Add files via upload

---
+++ Print_Hello_World_5_times_without_using_loop.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+using namespace std;
+class Print{
+    public:
+    Print(){
+        cout<<"Hello World! "<<endl;
+    }
+};
+int main(){
+    int n=5;
+    Print p[5];
+    return 0;
+}
