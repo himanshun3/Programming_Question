9ad86ac01ef3704ed7ebfcdeefb47873ad6159c9
Himanshu Narang <himanshunarang15@gmail.com>
2018-09-12 00:22:58 +0530

    Add files via upload

---
+++ Swap_2_numers_without_using_3rd_variable.cpp
@@ -0,0 +1,11 @@
+﻿#include<iostream>
+using namespace std;
+int main(){
+    int a =5;int b=4;
+    cout<<"values of a and b before swapping: "<<a<<" "<<b<<endl;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    cout<<"Values of a and b after swapping: "<<a<<" "<<b;
+    return 0;
+}
