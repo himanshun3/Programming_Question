ad9a0f7629ec1b0dc4dfe9ea43bfb22b6d1843c8
himanshun3 <himanshunarang15@gmail.com>
2018-09-27 04:06:29 +0530

    Given tree is binary search tree or not

---
+++ Trees/Given_tree_is_bst_or_not.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+struct node{
+    int data;
+    struct node* left;
+    struct node*  right;
+};
+struct node* newnode(int data){
+    struct node* new_node=(struct node*)malloc(sizeof(struct node));
+    new_node->data=data;
+    new_node->left=new_node->right=NULL;
+    return new_node;
+};
+bool isbst(struct node* root,struct node* l=NULL,struct node* r=NULL){
+    if(root==NULL){
+        return true;
+    }
+    if(l!=NULL && l->data>=root->data){
+        return false;
+    }
+    if(r!=NULL && r->data<=root->data){
+        return false;
+    }
+    return (isbst(root->left,l,root) && isbst(root->right,root,r));
+}
+int main(){
+    struct node* root=newnode(6);
+    root->left=newnode(4);
+    root->right=newnode(7);
+    root->left->left=newnode(3);
+    root->left->right=newnode(5);
+    if(isbst(root)){
+        cout<<"The given tree is a Binary Search Tree ";
+    }
+    else{
+        cout<<"The given tree is not a Binary Search Tree ";
+    }
+    return 0;
+}
