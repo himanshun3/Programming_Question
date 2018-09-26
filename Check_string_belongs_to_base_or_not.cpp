6e3a7564ca2ebc59898b640500acdf7b58374acf
himanshun3 <himanshunarang15@gmail.com>
2018-09-26 22:27:45 +0530

    Check whether the string belongs to a base or not

---
+++ Check_string_belongs_to_base_or_not.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+bool givenbase(string str,int base){
+    if(base>16){
+        return false;
+    }
+    else if(base<=10){
+        for(int i=0;i<str.length();i++){
+            if(!(str[i]>='0' && str[i]<=('0'+base))){
+                return false;
+            }
+        }
+    }
+    else{
+        for(int i=0;i<str.length();i++){
+            if(!((str[i]>='0' && str[i]<=('0'+base))||(str[i]>'A' && str[i]<=('A'+base-10)))){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+int main(){
+    string str="0101";
+    int base=2;
+    int ans=givenbase(str,base);
+    if(ans==1){
+        cout<<"Yes! The string "<<str<<" belongs to the base "<<base<<endl;
+    }
+    else{
+        cout<<"No! The string "<<str<<" does not belongs to the base "<<base<<endl;
+    }
+    return 0;   
+}
