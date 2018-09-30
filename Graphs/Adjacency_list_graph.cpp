741072cc64f1a7e934b89db292ab893f30faa558
himanshun3 <himanshunarang15@gmail.com>
2018-10-01 03:25:13 +0530

    Adjacency list Graphs

---
+++ Graphs/Adjacency_list_graph.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void addedge(vector<int> arr[],int u,int v){
+    arr[u].push_back(v);
+    arr[v].push_back(u);
+}
+void print(vector<int>arr[],int V){
+    for(int v=0;v<V;v++){
+        cout<<"Adjacency list for "<<v<<" is ";
+        for(auto it: arr[v]){
+            cout<<" -> "<<it;
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int V=5;
+    vector<int>arr[V];
+    addedge(arr,0,1);
+    addedge(arr,0,4);
+    addedge(arr,1,2);
+    addedge(arr,1,3);
+    print(arr,V);
+    return 0;
+}
