888f3a0ae197a0406d9d3cd62111e8083fd39a36
himanshun3 <himanshunarang15@gmail.com>
2018-10-01 04:21:14 +0530

    Converting string into characters

---
+++ Converting_string_into_characters.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<string>
+#include<bits/stdc++.h>
+using namespace std;
+void compare(string s1,string s2){
+    int m=s1.size();
+    int n=s2.size();
+    char a[m+1];
+    char b[n+1];
+    strcpy(a,s1.c_str());
+    strcpy(b,s2.c_str());
+    cout<<"First string is ";
+    for(int i=0;i<m;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"Second string is ";
+    for(int j=0;j<n;j++){
+        cout<<b[j]<<" ";
+    }
+}
+int main(){
+    string s1="Himanshu";
+    string s2="Narang";
+    compare(s1,s2);
+    return 0;
+}
