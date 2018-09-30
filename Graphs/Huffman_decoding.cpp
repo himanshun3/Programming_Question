d18f8939355cebe149d7487bbac47c3708224887
himanshun3 <himanshunarang15@gmail.com>
2018-10-01 03:30:59 +0530

    Huffman Decoding

---
+++ Graphs/Huffman_decoding.cpp
@@ -0,0 +1,15 @@
+﻿void decode_huffman(struct node * root, string s){
+    struct node* current=root;
+    for(int i=0;i<s.size();i++){
+        if(s[i]=='0'){
+            current=current->left;
+        }
+        else{
+            current=current->right;
+        }
+        if(current->left==NULL && current->right==NULL){
+            cout<<current->data;
+            current=root;
+        }
+    }
+}
