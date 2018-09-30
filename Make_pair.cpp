8d8f2b7e4b89b11de3cba0e669e06bb2f78d8e6e
himanshun3 <himanshunarang15@gmail.com>
2018-10-01 04:13:34 +0530

    Make pairs in a string

---
+++ Make_pair.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void printcount(string s){
+    map<pair<char,char>,int>dpair;
+    for(int i=0;i<s.length();i++){
+        dpair[make_pair(s[i],s[i+1])]++;
+    }
+    for(auto it=dpair.begin();it!=dpair.end();it++){
+        cout<<it->first.first<<it->first.second<<" -> "<<it->second<<endl;
+    }
+}
+int main(){
+    string s="abababbacacbcacbacabaca";
+    printcount(s);
+    return 0;
+}
