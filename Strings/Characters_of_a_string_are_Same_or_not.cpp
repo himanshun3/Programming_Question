e0a44a6190ea11ad97edf17c1627e3f8df791185
Himanshu Narang <himanshunarang15@gmail.com>
2018-10-02 03:21:56 +0530

    Characters of a string are same or not

---
+++ Strings/Characters_of_a_string_are_Same_or_not.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+bool compare(string str){
+    int n=str.length();
+    for(int i=1;i<n;i++){
+        if(str[i]!=str[i-1]){
+            return false;
+        }
+    }
+    return true;
+}
+int main(){
+    string str="gggg";
+    if(compare(str)){
+        cout<<"All the characters of the string are same ";
+    }
+    else{
+        cout<<"All the characters of the string are not same ";
+    }
+    return 0;
+}
