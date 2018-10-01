7d714ecb8f6d06e941d6874ec083084f38ad6370
Himanshu Narang <himanshunarang15@gmail.com>
2018-10-02 04:48:51 +0530

    Printing initials in a game

---
+++ Strings/Initials_in_a_name.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str="Himanshu Kumar Narang";
+    cout<<str[0]<<" ";
+    for(int i=0;i<str.length();i++){
+        if(str[i]==' '){
+            cout<<str[i+1]<<" ";
+        }
+    }
+    return 0;
+}
