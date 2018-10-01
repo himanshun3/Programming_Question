1db204caef0e01b2934ee4a8f027dbe0d6cc4e8c
Himanshu Narang <himanshunarang15@gmail.com>
2018-10-01 18:15:14 +0530

    copy strings using recursion

---
+++ Strings/copy_strings_using_recursion.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void copy(char s1[],char s2[],int i=0){
+    s2[i]=s1[i];
+    if(s1[i]=='\0'){
+        return;
+    }
+    copy(s1,s2,i+1);
+}
+int main(){
+    char s1[100]="Himanshu";
+    char s2[100]="";
+    copy(s1,s2);
+    cout<<s2<<" ";
+    return 0;
+}
