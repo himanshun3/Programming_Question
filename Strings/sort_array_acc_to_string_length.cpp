90c6670c167e3a00c79fa3528d42fc034f2ab7ec
Himanshu Narang <himanshunarang15@gmail.com>
2018-10-03 03:18:36 +0530

    Sort an array according to string length

---
+++ Strings/sort_array_acc_to_string_length.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+bool compare(string str1,string str2){
+    return str1.size()<str2.size();
+}
+void print(string str[],int n){
+    for(int i=0;i<n;i++){
+        cout<<str[i]<<" ";
+    }
+}
+int main(){
+    string arr[]={"Gurgaon","I","from","am"};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    sort(arr,arr+n,compare);
+    print(arr,n);
+    return 0;
+}
