5bfa6c2968ea709ec4f17a368ed1ef41ce8df7e6
Himanshu Narang <himanshunarang15@gmail.com>
2018-10-03 01:25:29 +0530

    Number of substrings of a string

---
+++ Strings/no_of_substrings_of_a_string.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+using namespace std;
+int noofsubstrings(string str){
+    int n=str.length();
+    int ans= n*(n+1)/2;
+    return ans+1;
+}
+int main(){
+    string str="abcd";
+    cout<<noofsubstrings(str);
+    return 0;
+}
