d222bf800a9589d821b45a8dbac48b31f45e8f9d
Himanshu Narang <himanshunarang15@gmail.com>
2018-10-03 03:58:25 +0530

    NAive Algorithm Pattern Searching

---
+++ Strings/Naive_Algorithm_Pattern_searching.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void naivesearch(char *txt,char *pat){
+    int m=strlen(txt);
+    int n=strlen(pat);
+    int i,j;
+    for(i=0;i<=m-n;i++){
+        for(j=0;j<n;j++){
+            if(txt[i+j]!=pat[j]){
+                break;
+            }
+        }
+        if(j==n){
+                cout<<"Pattern found at location "<<i<<endl;
+            }
+    }
+}
+int main(){
+    char txt[]="AABAACAADAABAAABAA";
+    char pat[]="AABA";
+    naivesearch(txt,pat);
+    return 0;
+}
