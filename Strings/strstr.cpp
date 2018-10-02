0b96922fab5d514a3a9a769b1bd2c645ad9d7729
Himanshu Narang <himanshunarang15@gmail.com>
2018-10-03 01:19:49 +0530

    StrStr in C++

---
+++ Strings/strstr.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include<string.h>
+using namespace std;
+int main(){
+    char str1[]="Geeksforgeeks";
+    char str2[]="for";
+    char *p;
+    p=strstr(str1,str2);
+    if(p){
+        cout<<"String Found"<<endl;
+        cout<<" First occurance of "<<str2<<" in "<<str1<<" is "<<p<<endl;
+    }
+    else{
+        cout<<"String not Found";
+    }
+    return 0;
+}
