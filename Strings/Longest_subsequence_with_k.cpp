8f3e9e7e5a466911c8a495d9b561c9b81522a1bf
Himanshu Narang <himanshunarang15@gmail.com>
2018-10-03 02:19:31 +0530

    Longest Subsequence where every character appears atleast k times

---
+++ Strings/Longest_subsequence_with_k.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+using namespace std;
+void longestsubsequencewithk(string str,int k){
+    int max_char=26;
+    int n=str.length();
+    int freq[max_char]={0};
+    for(int i=0;i<n;i++){
+        freq[str[i]-'a']++;
+    }
+    for(int i=0;i<n;i++){
+        if(freq[str[i]-'a']>=k){
+            cout<<str[i];
+        }
+    }
+}
+int main(){
+    string str="aabbaabacabb";
+    int k=2;
+    longestsubsequencewithk(str,k);
+    return 0;
+}
