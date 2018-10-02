af2cc134ba1f4f924dba5f47d8bbe62542d551b2
Himanshu Narang <himanshunarang15@gmail.com>
2018-10-02 18:13:12 +0530

    Removing spaces from a string

---
+++ Strings/Removing_spaces_from_a_string.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+using namespace std;
+char *removespaces(char *str){
+    int i=0;
+    int j=0;
+    while(str[i]){
+        if(str[i]!=' '){
+        str[j++]=str[i];
+        }
+        i++;
+    }
+    str[j]='\0';
+    return str;
+}
+int main(){
+    char str[]="ge e ks";
+    cout<<removespaces(str);
+    return 0;
+}
