17ddeb729a96dd232150f71890055233cf1b39f7
Himanshu Narang <himanshunarang15@gmail.com>
2018-10-07 21:41:41 +0530

    Print numbers between 100 and 1000 without using loop

---
+++ 100_and_1000_without_using_loop.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+using namespace std;
+class A{
+    public:
+    static int a;
+    A(){
+        while(a<1000){
+            cout<<a;
+            a++;
+        }
+    }
+};
+int A::a=100;
+int main(){
+    int N=900;
+    A obj[N];
+    return 0;
+}
