a852793c8bfea9edb1cd2538d693bc95fa77da90
Himanshu Narang <himanshunarang15@gmail.com>
2018-10-08 00:35:41 +0530

    Find the GCD of 2 numbers  using recursion

---
+++ GCD_of_2_numbers_using_recursion.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+int GCD(int x,int y){
+    int rem=x%y;
+    if(rem==0){
+        return y;
+    }
+    else{
+        return GCD(y,rem);
+    }
+}
+int main(){
+    int m=8,n=12;
+    cout<<GCD(m,n);
+    return 0;
+}
