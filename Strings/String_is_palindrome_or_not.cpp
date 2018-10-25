c8cb1f70901f0a1404e2344ba64a3dcbc221d8a3
Himanshu Narang <himanshunarang15@gmail.com>
2018-10-26 02:18:59 +0530

    String is palindrome or not

---
+++ Strings/String_is_palindrome_or_not.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include <string.h>
+using namespace std;
+void isPalindrome(char str[]) 
+{ 
+    int l = 0; 
+    int h = strlen(str) - 1; 
+    while (h > l) 
+    { 
+        if (str[l++] != str[h--]) 
+        { 
+            cout<<str<<" is not a palindrome "<<endl; 
+            return; 
+        } 
+    } 
+    cout<<str<<" is a palindrome "<<endl; 
+} 
+int main() 
+{ 
+    isPalindrome("abba"); 
+    isPalindrome("abbccbba"); 
+    isPalindrome("geeks"); 
+    return 0; 
+}
