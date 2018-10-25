74d117b23d03add7a1c836290079dd4593c72c1d
Himanshu Narang <himanshunarang15@gmail.com>
2018-10-26 01:17:12 +0530

    Implement queue using 2 stacks

---
+++ Implement_queue_using_2_stacks.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+struct Queue{
+  stack<int> s1,s2;
+  void enqueue(int x){
+      s1.push(x);
+  }
+  int dequeue(){
+      if(s1.empty() && s2.empty()){
+          cout<<"Queue is empty ";
+          exit(0);
+      }
+      if(s2.empty()){
+          while(!s1.empty()){
+              s2.push(s1.top());
+              s1.pop();
+          }
+      }
+      int x=s2.top();
+      s2.pop();
+      return x;
+  }
+};
+int main(){
+    Queue q;
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    cout<<q.dequeue()<<" ";
+    cout<<q.dequeue()<<" ";
+    cout<<q.dequeue()<<" ";
+    return 0;
+}
