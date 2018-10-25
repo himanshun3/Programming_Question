baf5df631e28eeec89f02f9e8d47db8357c8be43
Himanshu Narang <himanshunarang15@gmail.com>
2018-10-26 02:04:35 +0530

    Check whether a no is palindrome or not

---
+++ Check_whether_a_no_is_palindrome_or_not.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+using namespace std;
+int main(){
+    int num=121;
+    int n=num;
+    int r=0;
+    int digit;
+    while(num!=0){
+        digit=num%10;
+        r=(r*10)+digit;
+        num=num/10;
+    }
+    if(n==r){
+        cout<<"It is a palindrome ";
+    }
+    else{
+        cout<<"It is not a palindrome ";
+    }
+    return 0;
+}
