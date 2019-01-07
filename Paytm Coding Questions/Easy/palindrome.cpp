358a474fd565f410b54488477316524b2c7f2c4c
Himanshu Narang <himanshunarang15@gmail.com>
2019-01-07 17:45:04 +0530

    String is a palindrome or not

---
+++ Paytm Coding Questions/Easy/palindrome.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+#include<string.h>
+using namespace std;
+void ispalindrome(char str[]){
+    int l=0;
+    int h=strlen(str)-1;
+    while(h>l){
+        if(str[l++]!=str[h--]){
+            cout<<str<<" is not a palindrome "<<endl;
+            return;
+        }
+    }
+    cout<<str<<" is a palindrome "<<endl;
+}
+int main(){
+    ispalindrome("abba");
+    ispalindrome("abbbbac");
+    return 0;
+}
