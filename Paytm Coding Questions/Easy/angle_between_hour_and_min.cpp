6fac25ba0d25f33184546b60d322b52e32237620
Himanshu Narang <himanshunarang15@gmail.com>
2019-01-07 18:40:14 +0530

    Angle between hour and minute hand

---
+++ Paytm Coding Questions/Easy/angle_between_hour_and_min.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+using namespace std;
+void findangle(int h,int m){
+    if(h==12){
+        h=0;
+    }
+    if(m==60){
+        m=0;
+    }
+    int hour_angle=h*30+m*0.5;
+    int min_angle=m*6;
+    int angle=abs(hour_angle-min_angle);
+    angle=min(angle,360-angle);
+    cout<<angle;
+}
+int main(){
+    int h,m;
+    cout<<"Enter the hour "<<endl;
+    cin>>h;
+    cout<<"Enter the minutes "<<endl;
+    cin>>m;
+    findangle(h,m);
+    return 0;
+}
