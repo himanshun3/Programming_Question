3f3556f04f3dd9a377b7b3308881f1ee4eaf74e4
Himanshu Narang <himanshunarang15@gmail.com>
2019-01-09 18:23:29 +0530

    Count the number of paths from top left to bottom right of a m*n matrix

---
+++ Paytm Coding Questions/Easy/topleft_bottomright.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#include<stdio.h>
+using namespace std;
+int count(int m,int n){
+    if(m==1 || n==1){
+        return 1;
+    }
+    else{
+        return count(m-1,n)+count(m,n-1);  // if diagonals are also allowed then + count(m-1,n-1) 
+    }
+}
+int main(){
+    cout<<count(3,3);
+    return 0;
+}
