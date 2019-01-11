25d5ca9871877e6ba3db14a821eabc79a4a998e7
Himanshu Narang <himanshunarang15@gmail.com>
2019-01-12 02:56:12 +0530

    Remove adjacent duplicates recursively

---
+++ Paytm Coding Questions/Easy/remove_adjacent_duplicates.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<string.h>
+using namespace std;
+char *removedup(char *str){
+    int len=strlen(str);
+    int i=0;
+    int k=0;
+    for(int i=1;i<len;i++){
+        if(str[i-1]!=str[i]){
+            str[k++]=str[i-1];
+        }
+        else{
+            while(str[i-1]==str[i]){
+                i++;
+            }
+        }
+    }
+    str[k++]=str[i-1];
+    str[k]='\0';
+    return str;
+}
+int main(){
+    char str[]="geeksforgeek";
+    cout<<removedup(str);
+    return 0;
+}
