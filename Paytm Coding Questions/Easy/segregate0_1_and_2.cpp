24078712ff3d0ec7c16884a77f6ba07fb4068dfa
Himanshu Narang <himanshunarang15@gmail.com>
2019-01-16 02:28:21 +0530

    Segregate 0,1 and 2

---
+++ Paytm Coding Questions/Easy/segregate0_1_and_2.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+#include<stdio.h>
+using namespace std;
+void segregate(int arr[],int n){
+    int count0=0,count1=0,count2=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]==0){
+            count0++;
+        }
+        else if(arr[i]==1){
+            count1++;
+        }
+        else if(arr[i]==2){
+            count2++;
+        }
+    }
+    for(int i=0;i<count0;i++){
+        arr[i]=0;
+    }
+    for(int i=count0;i<(count0+count1);i++){
+        arr[i]=1;
+    }
+    for(int i=(count0+count1);i<(count0+count1+count2);i++){
+        arr[i]=2;
+    }
+}
+int main(){
+    int arr[]={0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    segregate(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
